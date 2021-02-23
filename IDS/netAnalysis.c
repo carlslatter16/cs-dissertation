e6749c57535af0b5a0ee29a98cd5dc71307f6e7b
carlslatter16 <34287567+carlslatter16@users.noreply.github.com>
2021-02-23 16:03:25 +0000

    Adding start of domain counter

--- IDS/netAnalysis.c
+++ IDS/netAnalysis.c
@@ -2,12 +2,21 @@
 #include <stdlib.h>
 #include <stdbool.h> 
 #include <string.h>
+#include <unistd.h>
 
 int main (int argc, char *argv[])
 {
     char line[100];
+    char Fileline[100];
     char subdomain[50];
     char domainSource[50];
+
+    char domainList[] = "domainsFound.txt";
+
+    FILE *domainListFile = fopen(domainList, "a+");
+
+
+    char dnsTrafficLog[] = "file.txt";
     
     int domSrcIndex;
     int lenThreshold = 8;
@@ -23,44 +32,58 @@ int main (int argc, char *argv[])
 
     bool base64Bool = false;
    
+    char domainSets[200];
+    int domainSourceNum; //tricky to assign to a specific domain name when there will be multiple
+    int domainSourceThreshold = 5;
+
     int abnormalityThreshold = 2;
 
     //can play around with the thresholds
 
-    FILE *f = fopen("file.txt", "r");
+    //https://stackoverflow.com/questions/230062/whats-the-best-way-to-check-if-a-file-exists-in-c
+    //https://www.geeksforgeeks.org/c-program-delete-file/
+    if( access( domainList, F_OK ) == 0 ) {
+        if (remove(domainList) == 0) {
+            printf("Deleted the temporary files from the previous session   \n\n");
+        }
+        else {
+        printf("Unable to delete the previous domain parsing log  \n\n");
+        }
+    }
+
+    FILE *f = fopen(dnsTrafficLog, "r");
 
     if (f == NULL) {
         printf("Error opening file!\n");
         exit(1);
     }
 
-    while( fgets(line, 100, f) != NULL ) {
+    while( fgets(line, 100, f) != NULL ) { //get each line
         int i;
         int x;
         
-        for (i = 0; line[i] != '\0'; i++) {
+        for (i = 0; line[i] != '\0'; i++) { //cycle through each char of each line of log
             if(line[i]!=46) {  //collects the subdomain up till the '.'
                 snprintf(subdomain, 50, "%c", line[i]); //assumption that the left most label is the malicous part, could be mixed up.
                 
-                if(line[i]==61) {
+                if(line[i]==61) { //if '=' is found
                     base64Bool = true;
                 }
 
-                if(line[i] >= 65 && line[i] <= 90) {
+                if(line[i] >= 65 && line[i] <= 90) { //if ascii "A-Z" is found
                     capCount++;
                     if(capCount >= capThreshold) {
                         capThresholdBool = true;
                     }
                 }
 
-                if(isdigit(line[i])) {
+                if(isdigit(line[i])) { //if a number is found
                     numCount++;
                     if(numCount >= numThreshold) {
                         numThresholdBool = true;
                     }
                 }
             }
-
             else {
                 domSrcIndex = i + 1; 
                 break;
@@ -72,57 +95,113 @@ int main (int argc, char *argv[])
 
         int domSrcIndex = i + 1;
 
+        
+
+        if (domainListFile == NULL) { //stuff like this could be functioned down
+            printf("Error opening file!\n");
+            exit(1);
+        }
+
         for(domSrcIndex; line[domSrcIndex]!='\0'; domSrcIndex++) { //collects everything after the subdomain to check for a constant
             //printf('%d', domSrcIndex);
             snprintf(domainSource, 50, "%c", line[domSrcIndex]);
             printf(domainSource);
+            fprintf(domainListFile, domainSource);
         }
+
 
 
         int abnormalityCount = 0;
 
-        if(i-1>=lenThreshold) { //if too long to be normal
-            printf("   Abnormal length of subdomain fragment!");
+        //could show end count?
+        
+        if(domainSourceNum >= domainSourceThreshold) { //if there are too many of the same domain in the log
+            printf("   Abnormal amount of request to the same domain: ");
+            printf("%d", domainSourceNum);
+            abnormalityCount++;
+        }
+
+        if(i-1 >= lenThreshold) { //if too long to be normal
+            printf("   Abnormal length of subdomain fragment: ");
+            printf("%d", i-1);
             abnormalityCount++;
         }
 
         if(base64Bool==true) { //if = was found
             base64Bool=false;
             printf("   Possible Base64!");
+            //could decode
+            //could then analysis the decode - if bytes etc..
             abnormalityCount++;
         }
 
         if(capThresholdBool==true) { //if too many caps to be normal
             capThresholdBool = false;
-            printf("   Abnormal occurances of capitals!");
+            printf("   Abnormal occurances of capitals: ");
+            printf("%d", capCount);
             abnormalityCount++;
         }
 
         if(numThresholdBool==true) { //if too many numbers to be normal
             numThresholdBool = false;
-            printf("   Abnormal occurances of numbers!");
+            printf("   Abnormal occurances of numbers: ");
+            printf("%d", numCount);
             abnormalityCount++;
-            printf("\n");
         }
 
         //could look at reused domains again - 
         //if there are lots of requests
         //if there are a lot of failed server responces - maybe need to log them or something to then analyse here!
 
+
+        //i'm currently doing per packet analysis, could tie some together and comapre domains
+
         if(abnormalityCount>=abnormalityThreshold) {
+            printf("\n");
             printf("   # THIS IS LIKELY A MALICIOUS UDP DNS PACKET! # ");
             printf("\n");
-            //write to file?
+            //write to file with all the stored parameters if not 0 or false?
             //read from pcap instead - might be a bit out there for now
+            //add better logging to the other one - timestamps etc..
+        }
+        else {
+            printf("\n");
+            printf("   # THIS IS LIKELY NOT MALICIOUS DNS # ");
+            printf("\n");
         }
 
 
         printf("%c", '\n');
         capCount = 0;
-        
+        numCount = 0;
+        domainSourceNum = 0;
+
         memset(subdomain, 0, sizeof(subdomain));
         memset(domainSource, 0, sizeof(domainSource));
     }
-      
+
+    char ch;
+    int lineCount = 0;
+        
+
+    while(!feof(domainListFile)) { //https://stackoverflow.com/questions/12733105/c-function-that-counts-lines-in-file
+        ch = fgetc(domainListFile);
+        if(ch == '\n')
+        {
+            lineCount++;
+        }
+    }
+
+    //!!! Stopped here - Trying to count lines in a file to find upper range to compare new entries to 
+
+    printf('%d', lineCount);
+
+    //currentDomain = fgets(FileLine, 256, domainListFile);
+    int z;
+
+    //can then delete original file used by IDS
+    //link it so that on ctrl c its automatic for analysis somehow?
+
+    fclose(domainListFile);
     fclose(f);
 }

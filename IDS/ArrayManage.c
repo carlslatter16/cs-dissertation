8e3c2c68ba38a72609c3bc71d2e7708c5b049704
carlslatter16 <34287567+carlslatter16@users.noreply.github.com>
2021-02-28 07:30:26 -0800

    Added array insert poc

--- IDS/ArrayManage.c
+++ IDS/ArrayManage.c
@@ -6,35 +6,30 @@
 
 char* array[20];
 bool firstRun = true;
-int size = sizeof(array) / sizeof(array[0]);
 
-int insertElement(char currentString[]) 
+int insertElement(char* stringToAdd, int size)
 {
     int x=0;
-    int z=0;
     bool newValue;
     int emptyIndex;
 
-     
+
     //https://stackoverflow.com/questions/2427336/why-cant-i-create-an-array-with-size-determined-by-a-global-variable
     //finds size based on overall size of the array, and then by a sample element
-
+    
     if(firstRun==true) {
-        array[0] = currentString;
+        array[0] = stringToAdd; //could me moved to main
         firstRun=false;
-    }
-
-    for(z=0;z<size;++z)
-    {
-        array[z] = "\0";
     }
 
     for(x=0;x<size;++x) {
         if(array[x]!="\0") {
-            if(array[x]!=currentString) {
+            if(array[x]!=stringToAdd) {
                 newValue=true;
             }
-            
+            else {
+                newValue=false;
+            }
         }
         else{
             emptyIndex=x;
@@ -42,28 +37,39 @@ int insertElement(char currentString[])
         }
     }
 
-    if(newValue==true) {
-        array[emptyIndex] = currentString;
-        newValue=false;
+    if(newValue!=false) {
+        array[emptyIndex] = stringToAdd;
+        newValue=NULL;
         return 1;  
     }
+
+    return 0;
 }
 
 int main (int argc, char *argv[]) 
 {   
-    if(insertElement("pokemon")==1) {
-        if(insertElement("pikachu")==1) {
-            if(insertElement("ash")==1) {
-                return 1;
-            }
-        }
+    int size = sizeof(array) / sizeof(array[0]);
+    char* currentString = "Pokemon";
+    int z;
+    
+    for(z=0;z<size;++z)
+    {
+        array[z] = "\0";
     }
+
+    insertElement(currentString, size);
+    currentString = "Pikachu";
+    insertElement(currentString, size);
+    insertElement(currentString, size);
+    currentString = "Eevee";
+    insertElement(currentString, size);
 
     int p;
 
     for(p=0;p<=size;++p) {
-        printf("%s",array[p]); //hard to print
+        if(array[p]!=NULL) {
+            printf("%s", array[p]); //hard to print
+        }
     }
-
     return 0;
 }
